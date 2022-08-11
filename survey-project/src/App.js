3317b9f0abcdba4e2a0c0ff000c65dbbfd53ce18
Veeramanikandan-R-1 <r.veeramanikandany216@gmail.com>
2022-08-11 12:41:18 +0530

    router

--- survey-project/src/App.js
+++ survey-project/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/Login";
 
 const App = () => {
-  return <div>Survey Project</div>;
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
 };
 
 export default App;

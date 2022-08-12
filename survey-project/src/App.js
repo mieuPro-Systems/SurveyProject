9d17e8f318c7bbf44224af7a8a416ec96276d8d2
hanishkumar606 <hanishkumar606@gmail.com>
2022-08-12 11:34:49 +0530

    assessts

--- survey-project/src/App.js
+++ survey-project/src/App.js
@@ -3,22 +3,18 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import LoginScreen from "./screens/Login";
-<<<<<<< HEAD
 import DrawerComponent from "./components/common/SideDrawer";
 import AddEmployeeScreen from "./screens/AddEmployee";
 import ViewEmployeesScreen from "./screens/ViewEmployee";
-=======
 import HomeScreen from "./screens/Home";
->>>>>>> e7fab319275dd7d86e2b3dc61ed379b6eb5d4468
 
 const App = () => {
   const { isAuthenticated } = useSelector((state) => state.auth);
 
   return (
-<<<<<<< HEAD
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginScreen />} />
+        <Route path="/" element={<HomeScreen />} />
         <Route path="/login" element={<LoginScreen />} />
         <Route
           path="/dashboard"
@@ -31,16 +27,6 @@ const App = () => {
         </Route>
       </Routes>
     </BrowserRouter>
-=======
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
->>>>>>> e7fab319275dd7d86e2b3dc61ed379b6eb5d4468
   );
 };
 

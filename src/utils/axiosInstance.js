8f3d5570c94cc0f9f3070bd5ac63124128aa21ed
hanishkumar606 <hanishkumar606@gmail.com>
2023-02-06 12:57:18 +0530

    update url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-const axiosInstance = axios.create({ baseURL: "https://3f3c-49-204-119-161.in.ngrok.io" });
+const axiosInstance = axios.create({ baseURL: "https://3120-49-204-143-128.in.ngrok.io" });
 export default axiosInstance;
